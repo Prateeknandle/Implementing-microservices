3146c2e9c806b25009f03fcbef8932d06d1bf17d
Prateeknandle <prateeknandle@gmail.com>
2022-04-13 21:00:20 +0530

    request handling improved

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Prateeknandle/Implementing-microservices/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
-	//handles request
-	http.ListenAndServe(":9098", sm) // sm = serveMux
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9098",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	//handles request in go func() so it not gets blocked
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
